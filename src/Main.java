e21005ffb5647b27492ae06736db48429f0e1148
seungkyeong <skyeong1003@naver.com>
2025-11-23 09:49:59 +0900

    man commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
+            System.out.println("main");
             System.out.println("i = " + i);
         }
     }
